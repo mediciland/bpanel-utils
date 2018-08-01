356457e685a6e3026dabbb5ecb560be446c45d4a
bucko13 <bucko.perley@gmail.com>
2018-08-01 10:07:44 -0700

    add functionality to getHash helper

--- test/helpers-test.js
+++ test/helpers-test.js
@@ -1,12 +1,14 @@
 import { expect } from 'chai';
+import bcrypto from 'bcrypto';
 
 import { getHash } from '../lib/helpers';
 
-describe.only('helpers', () => {
+describe('helpers', () => {
   describe('getHash', () => {
     it('should hash stringified versions of non-string inputs', () => {
       const obj = { test: 'test' };
       const stringified = JSON.stringify(obj);
+      console.log('stringified:', stringified);
       const objHash = getHash(obj);
       const stringHash = getHash(stringified);
       expect(objHash).to.equal(stringHash);
@@ -19,6 +21,21 @@ describe.only('helpers', () => {
       test = `${test} `;
       const newHash = getHash(test);
       expect(newHash).to.not.equal(testHash);
+    });
+
+    it('should hash a buffer', () => {
+      const test = Buffer.from('test', 'ascii');
+      const hash = getHash(test, 'sha256');
+      const expected = bcrypto.sha256.digest(test);
+      expect(hash).to.equal(expected.toString('hex'));
+    });
+
+    it('should be able to use other hashing algos', () => {
+      const hashFunc = 'md5';
+      const preimage = Buffer.from('test', 'ascii');
+      const actual = getHash(preimage, hashFunc);
+      const expected = bcrypto[hashFunc].digest(preimage);
+      expect(actual).to.equal(expected.toString('hex'));
     });
   });
 });
